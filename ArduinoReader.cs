7d805af440f1d003c55c98e1c6a696ffed1ce29f
TheDilz0 <43611329+TheDilz0@users.noreply.github.com>
2018-09-26 16:17:03 +0200

    Add files via upload

---
+++ ArduinoReader.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO.Ports;
+using System.IO;
+//
+using UnityEngine.UI;
+
+public class ArduinoReader : MonoBehaviour
+{
+	SerialPort sp = new SerialPort("COM4", 9600, Parity.None, 8, StopBits.One);
+
+	public int nbVars;
+
+	public Canvas c;
+	public Text[] t;
+	int n = 0;
+
+	void Start()
+	{
+		sp.Open();
+		sp.ReadTimeout = 1;
+		t = c.GetComponentsInChildren<Text>();
+	}
+
+	private void Update()
+	{
+		if (sp.IsOpen)
+		{
+
+			try
+			{
+				int nb = sp.ReadByte() * 4;
+				t[n].text = nb.ToString();
+
+				if (n+1 >= nbVars)
+				{
+					n = 0;
+				}
+				else
+				{
+					n += 1;
+					//print("reset");
+				}
+
+				sp.BaseStream.Flush();
+				
+			}
+			catch (System.Exception)
+			{
+				//throw;
+			}
+		}
+	}
+}
+
+/*
+ 
+	using UnityEngine;
+using System.Collections;
+using System.IO.Ports;
+using System.IO;
+//
+using UnityEngine.UI;
+
+public class ArduinoReader : MonoBehaviour
+{
+	SerialPort sp = new SerialPort("COM3", 9600, Parity.None, 8, StopBits.One);
+	public float[] Sensors;
+
+	//vars
+	public string value;
+	public string[] ArdVar;
+	public int p;
+
+	//
+	public Canvas c;
+	public Text[] t;
+	//
+
+	void Start()
+	{
+		sp.Open();
+		sp.ReadTimeout = 10;
+		t = c.GetComponentsInChildren<Text>();
+	}
+
+	private void Update()
+	{
+		if (sp.IsOpen)
+		{
+
+			try
+			{
+				value = sp.ReadLine();
+
+				ArdVar = value.Split('/');
+
+				foreach(string i in ArdVar)
+				{
+					string[] result = i.Split(':');
+					Sensors[int.Parse(result[0])] = float.Parse(result[1]);
+				}
+				
+				foreach (float f in Sensors)
+				{
+					p = System.Array.IndexOf(Sensors, f);
+					//
+					t[p].text = f.ToString();
+				}
+				
+				sp.BaseStream.Flush();
+				
+			}
+			catch (System.Exception)
+			{
+				//throw;
+			}
+		}
+	}
+}
+
+  */
---
+++ MixTableV3.ino
@@ -0,0 +1,32 @@
+#include <Velleman_KA12.h>
+
+int all[24];
+int sensor1;
+
+void setup() {
+  Serial.begin(9600);
+  ka12_init(); 
+}
+
+void loop() {
+  ka12_readAll(all);
+  for (int i=0; i < 24; i=i+1) {
+  //send
+  Serial.write(all[i]/4);
+  /*
+  if(i!=23){
+    Serial.print("/"); 
+    }
+  }
+  Serial.println();
+  */
+  delay(100);
+  
+}
+}
+
+/*
+  sensor1 = ka12_read(6);
+  Serial.print("Sensor 1 :");
+  Serial.println(sensor1); 
+  */
